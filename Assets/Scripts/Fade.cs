dcfb1b5f52c6587db988068c0426015d71da981d
Artem Pluzhnyk <pluzhnyk.artem@gmail.com>
2020-06-01 21:12:27 +0300

    Last push

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+	public float fadeSpeed = 1.25f;
+	private int fadeDirection = -1;
+
+	private Color colorGUI = Color.white;
+
+	private void OnGUI()
+	{
+		colorGUI.a = Mathf.Clamp01(colorGUI.a + fadeDirection * fadeSpeed * Time.deltaTime);
+
+		GUI.depth = -1000;
+		GUI.color = colorGUI;
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), Texture2D.whiteTexture);
+	}
+
+	public float BeginFade(int direction)
+	{
+		fadeDirection = direction;
+		return (1 / fadeSpeed);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float force = 200f;
+
+	public string[] keywords = new string[] {"left", "right"};
+	public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+	protected PhraseRecognizer recognizer;
+
+	void Start()
+	{
+		if (keywords != null)
+		{
+			recognizer = new KeywordRecognizer(keywords, confidence);
+			recognizer.OnPhraseRecognized += OnKeywordRecognized;
+			recognizer.Start();
+		}
+	}
+
+	private void OnKeywordRecognized(PhraseRecognizedEventArgs args)
+	{
+		if (args.text == "left")
+			TurnLeft(force * 5);
+		if (args.text == "right")
+			TurnRight(force * 5);
+	}
+
+	private void OnApplicationQuit()
+	{
+		if (recognizer != null && recognizer.IsRunning)
+			recognizer.Stop();
+	}
+
+	private void TurnRight(float force)
+	{
+		Rigidbody player;
+		player = GetComponent<Rigidbody>();
+
+		player.AddForce(force * Time.deltaTime, 0f, 0f, ForceMode.VelocityChange);
+	}
+
+	private void TurnLeft(float force)
+	{
+		Rigidbody player;
+		player = GetComponent<Rigidbody>();
+
+		player.AddForce(-force * Time.deltaTime, 0f, 0f, ForceMode.VelocityChange);
+	}
+	private void FixedUpdate()
+	{
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+			TurnRight(force);
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+			TurnLeft(force);
+	}
+}
